7cc168f3924b3fc488406c873960be8189fcf1d3
Arturo G. Bruno <arturogbruno@gmail.com>
2024-09-27 12:41:51 +0200

    feat: implement PostShow page

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,5 +1,27 @@
-const PostShowPage = () => {
-  return <div>Post Show Page</div>;
+import CommentCreateForm from '@/components/comments/comment-create-form';
+import PostShow from '@/components/posts/post-show';
+import paths from '@/paths';
+import Link from 'next/link';
+
+interface PostShowPageProps {
+  params: {
+    postId: string;
+    slug: string;
+  };
+}
+
+const PostShowPage = ({ params }: PostShowPageProps) => {
+  const { postId, slug } = params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {'< '}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+      <CommentCreateForm postId={postId} startOpen />
+    </div>
+  );
 };
 
 export default PostShowPage;
